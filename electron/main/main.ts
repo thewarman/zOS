6dd7c98017f9c299d688f75eb2f47f56dcc29f23
thewarman <warmanx@protonmail.com>
2024-01-28 21:34:42 -0500

    ci: bump

--- electron/main/main.ts
+++ electron/main/main.ts
@@ -25,7 +25,10 @@ function setupIpc() {
   });
 }
 
-function onBeforeQuit() {}
+function onBeforeQuit() {
+  log.info('onBeforeQuit');
+  app.quit();
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
